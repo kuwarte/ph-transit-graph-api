a062c31beacb74578dacc300251b59c96c7398db
Jasper Cuarte <hekiigyokuu@gmail.com>
2026-06-25 21:22:00 +0800

    add route jpa repository

---
+++ src/main/java/com/phtransitgraph/repository/RouteRepository.java
@@ -0,0 +1,20 @@
+package com.phtransitgraph.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.phtransitgraph.entity.Route;
+import com.phtransitgraph.enums.VehicleType;
+
+@Repository
+public interface RouteRepository extends JpaRepository<Route, String> {
+    Optional<Route> findByRouteCode(String routeCode);
+
+    List<Route> findByVehicleType(VehicleType vehicleType);
+
+    List<Route> findByOriginAndDestinationContainingIgnoreCase(String origin, String destination);
+
+}
