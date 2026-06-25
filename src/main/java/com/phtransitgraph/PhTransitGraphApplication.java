4ead47931e82d64db36647de6145cfa2679ba6e7
Jasper Cuarte <hekiigyokuu@gmail.com>
2026-06-25 16:04:18 +0800

    init project setup

---
+++ src/main/java/com/phtransitgraph/PhTransitGraphApplication.java
@@ -0,0 +1,13 @@
+package com.phtransitgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhTransitGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhTransitGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phtransitgraph/PhTransitGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phtransitgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhTransitGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
