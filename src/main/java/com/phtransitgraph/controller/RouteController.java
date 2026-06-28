619a54a397a5cf1e41f96dd55ef2abb7ae6abc24
Jasper Cuarte <hekiigyokuu@gmail.com>
2026-06-28 22:15:44 +0800

    update route pagination

--- src/main/java/com/phtransitgraph/controller/RouteController.java
+++ src/main/java/com/phtransitgraph/controller/RouteController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import com.phtransitgraph.dto.request.RouteRequest;
+import com.phtransitgraph.dto.response.PageResponse;
 import com.phtransitgraph.dto.response.RouteResponse;
 import com.phtransitgraph.service.RouteService;
 import jakarta.validation.Valid;
